c2310337172e0225ec04c57fac371993d18dbe0b
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2023-04-11 00:32:46 +0300

    create custom JPQL query to fetch user with roles

--- Baigiamasis/Back-end/src/main/java/com/example/Backend/entities/User.java
+++ Baigiamasis/Back-end/src/main/java/com/example/Backend/entities/User.java
@@ -37,7 +37,7 @@ public class User implements UserDetails {
     @Column(name = "password")
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany
     private Set<Role> roles;
 
 

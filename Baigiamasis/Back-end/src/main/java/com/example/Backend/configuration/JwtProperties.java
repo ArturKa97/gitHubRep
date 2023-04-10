f8aff228815eb5cb88a299c7e5ab93149b360208
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2023-04-11 00:28:55 +0300

    Validation and default values for ConfigurationProperties

--- Baigiamasis/Back-end/src/main/java/com/example/Backend/configuration/JwtProperties.java
+++ Baigiamasis/Back-end/src/main/java/com/example/Backend/configuration/JwtProperties.java
@@ -1,12 +1,17 @@
 package com.example.Backend.configuration;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @Data
-@ConfigurationProperties(prefix = "eshop.jwt")
+@Validated
+@ConfigurationProperties(prefix = "nutrition.jwt")
 public class JwtProperties {
-    private long validTimeInSeconds;
+    private long validTimeInSeconds = 1800;
+
+    @NotBlank
     private String secretKey;
 
     public long getValidTimeInMillis() {

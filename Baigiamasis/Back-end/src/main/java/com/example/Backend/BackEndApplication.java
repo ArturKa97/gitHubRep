2dbf70eb577c7696214ea3f57e31e7474988b98c
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2023-04-11 00:30:49 +0300

    use Component to create bean from properties class

--- Baigiamasis/Back-end/src/main/java/com/example/Backend/BackEndApplication.java
+++ Baigiamasis/Back-end/src/main/java/com/example/Backend/BackEndApplication.java
@@ -1,11 +1,8 @@
 package com.example.Backend;
 
-import com.example.Backend.configuration.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(JwtProperties.class)
 @SpringBootApplication
 public class BackEndApplication {
 
--- Baigiamasis/Back-end/src/main/java/com/example/Backend/configuration/JwtProperties.java
+++ Baigiamasis/Back-end/src/main/java/com/example/Backend/configuration/JwtProperties.java
@@ -3,10 +3,12 @@ package com.example.Backend.configuration;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 @Data
 @Validated
+@Component
 @ConfigurationProperties(prefix = "nutrition.jwt")
 public class JwtProperties {
     private long validTimeInSeconds = 1800;

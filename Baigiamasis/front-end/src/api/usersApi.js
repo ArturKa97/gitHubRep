6fb6f67d079a8030bdd7b9290940b273145adea0
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2023-04-21 14:46:34 +0300

    Calling backend for login

--- Baigiamasis/front-end/src/api/usersApi.js
+++ Baigiamasis/front-end/src/api/usersApi.js
@@ -1,3 +1,6 @@
 import HTTP from "./";
 
-const login = (loginData) => HTTP.post("/user/login", loginData);
+const login = (loginData) => HTTP.post("/user/login", loginData)
+    .then(({ data }) => data)
+
+export { login }

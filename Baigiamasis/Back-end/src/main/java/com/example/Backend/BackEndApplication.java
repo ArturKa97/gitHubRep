b9d5086010f3916c66c8fe2d8cc6b04ab25ad0b3
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2023-05-09 11:43:15 +0300

    Allowing access to H2 console

--- Baigiamasis/Back-end/src/main/java/com/example/Backend/BackEndApplication.java
+++ Baigiamasis/Back-end/src/main/java/com/example/Backend/BackEndApplication.java
@@ -2,8 +2,11 @@ package com.example.Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.h2.H2ConsoleProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(H2ConsoleProperties.class)
 public class BackEndApplication {
 
 	public static void main(String[] args) {
--- Baigiamasis/Back-end/src/main/java/com/example/Backend/configuration/SecurityConfig.java
+++ Baigiamasis/Back-end/src/main/java/com/example/Backend/configuration/SecurityConfig.java
@@ -2,17 +2,21 @@ package com.example.Backend.configuration;
 
 import com.example.Backend.security.JwtAuthorizationFilter;
 import com.example.Backend.services.JwtService;
+import org.springframework.boot.autoconfigure.h2.H2ConsoleProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
@@ -42,6 +46,14 @@ public class SecurityConfig {
                 http.addFilterBefore(new JwtAuthorizationFilter(jwtService), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(H2ConsoleProperties properties) {
+
+        return web -> web
+                .ignoring()
+                .requestMatchers(properties.getPath() + "/**");
     }
 
     @Bean
